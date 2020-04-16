812a2cec04150c3a24be6bf91ae6b3a0a92f4c26
kariDom <59494659+FantaSoda@users.noreply.github.com>
2020-04-15 21:17:44 -0400

    Update TistaSectionC.java

--- src/main/java/TistaSectionC.java
+++ src/main/java/TistaSectionC.java
@@ -1,6 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.Assert;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,31 +8,38 @@ import java.util.List;
 public class TistaSectionC {
     String sentence;
     String word;
+    String expectedWithoutfirstAndLAst;
+    String expectedlongest;
     List<Integer> nums;
+     List<Integer>positiveNums;
 
     @Before
     public void varSetUp() {
         sentence = "Wiktionary has grown beyond a standard dictionary and now includes a thesaurus, a rhyme guide, phrase books, language statistics and extensive appendices. ";
+       expectedlongest="Wiktionary and 10";
         word = "CoddingJava";
+        expectedWithoutfirstAndLAst= "oddingJav";
         nums = new ArrayList<Integer>(Arrays.asList(5, 6, 3, 2, 0, -5, -33));
+         positiveNums= new ArrayList<Integer>(Arrays.asList(5, 6, 3, 2, 0));
 
     }
 
     @Test
     public void takeoutFirstAndLastTest() {
         System.out.println(TistaSectionB.takeoutFirstAndLast(word));
-        assertEquals("oddingJav",TistaSectionB.takeoutFirstAndLast(word));
+        Assert.assertEquals(expectedWithoutfirstAndLAst,TistaSectionB.takeoutFirstAndLast(word));
     }
 
     @Test
     public void getNotNullsTest() {
         System.out.println(TistaSectionB.getNotNulls(nums));
-         assertEquals([],TistaSectionB.getNotNulls(nums));
+          Assert.assertEquals(positiveNums,TistaSectionB.getNotNulls(nums));
     }
 
     @Test
     public void getTheLongestTest() {
         System.out.println(TistaSectionB.getTheLongest(sentence));
+            Assert.assertEquals(expectedlongest, TistaSectionB.getTheLongest(sentence));
     }
 
     /*
@@ -59,13 +66,13 @@ public class TistaSectionC {
     @Test
     public void getTheShortest() {
         System.out.println(getTheShortest(sentence));
-        assertEquals("a",getTheShortest(sentence));
+        Assert.assertEquals("a and 1",getTheShortest(sentence));
 
     }
 
     @Test
     public void getShortest() {
         System.out.println(getTheShortest("Tinkle, Tinkle little car How I wonder what you are Leaking oil every day Having it your own way."));
-         assertEquals("I",getTheShortest("Tinkle, Tinkle little car How I wonder what you are Leaking oil every day Having it your own way."));
+         Assert.assertEquals("I and 1",getTheShortest("Tinkle, Tinkle little car How I wonder what you are Leaking oil every day Having it your own way."));
     }
 }
